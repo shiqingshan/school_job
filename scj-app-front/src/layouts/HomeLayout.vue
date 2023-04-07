ce1737a645774ace33f7a8480cdede57d9299c5a
shiqingshan <1147863825@qq.com>
2023-04-07 18:01:02 +0800

    a
    app

---
+++ scj-app-front/src/layouts/HomeLayout.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <n-layout>
+            <n-layout-header>颐和园路</n-layout-header>
+            <n-layout-content content-style="padding: 24px;">
+                <router-view></router-view>
+            </n-layout-content>
+            <n-layout-footer>成府路</n-layout-footer>
+          </n-layout>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ scj-app-front/src/router/index.ts
@@ -0,0 +1,20 @@
+import {RouteRecordRaw, createRouter,createWebHistory} from 'vue-router';
+const routes:Array<RouteRecordRaw> =[{
+    path: '/',
+    name: 'index',
+    component: ()=> import("@/layouts/HomeLayout.vue"),
+    children:[
+        {
+            path: 'p1',
+            name: 'p1',
+            component: ()=> import("@/components/HelloWorld.vue")
+        }
+    ]
+}
+]
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
